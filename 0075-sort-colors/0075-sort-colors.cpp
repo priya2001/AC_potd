f77b2709efdb9f13c5e343d4c9456a69c2bc653e
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-01-17 17:20:19 +0530

    Time: 4 ms (33.22%), Space: 8.6 MB (81.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,35 +4,62 @@ public:
         // ###################brute force approach
         // sort(nums.begin(),nums.end());
         
-        // **************************better approach
-        int count0=0,count1=0,count2=0;
-        for(int i=0;i<nums.size();i++)
+        
+        // **************************better approach  O(2N) time complexity
+//         int count0=0,count1=0,count2=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]==0)
+//             {
+//                 count0++;
+//             }
+//             else if(nums[i]==1)
+//             {
+//                 count1++;
+//             }
+//             else
+//             {
+//                  count2++; 
+//             }
+//         }
+//         for(int i=0;i<count0;i++)
+//         {
+//             nums[i]=0;
+//         }
+//         for(int i=count0;i<count0+count1;i++)
+//         {
+//             nums[i]=1;
+//         }
+//         for(int i=count0+count1;i<nums.size();i++)
+//         {
+//             nums[i]=2;
+//         }
+        
+//         }
+        
+    // **********************************Optimal appraoch O(N) 
+        
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high)
         {
-            if(nums[i]==0)
+            if(nums[mid]==0)
             {
-                count0++;
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            else if(nums[i]==1)
+            else if(nums[mid]==1)
             {
-                count1++;
+                mid++;
             }
             else
             {
-                 count2++; 
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
-        for(int i=0;i<count0;i++)
-        {
-            nums[i]=0;
-        }
-        for(int i=count0;i<count0+count1;i++)
-        {
-            nums[i]=1;
-        }
-        for(int i=count0+count1;i<nums.size();i++)
-        {
-            nums[i]=2;
-        }
         
-        }
+        
+    }
+        
 };
