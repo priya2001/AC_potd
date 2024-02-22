fb7b7f4f5a08ec5b027121df1cc38d4866d33835
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-02-23 01:09:11 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (39.67%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -9,32 +9,62 @@
  * };
  */
 class Solution {
+    
 public:
-    ListNode *reverse(ListNode *head)
-    {
-        ListNode *current=head;
-        ListNode *prev=NULL,*next=NULL;
-        while(current!=NULL)
-        {
-            next=current->next;
-            current->next=prev;
-            prev=current;
-            current=next;
-        }
-        head=prev;
-        return head;
-    }
+    
+            // ----------------------------------------Reverse Approach----------------------------
+//      ListNode *reverse(ListNode *head)
+//    {   
+        
+//         ListNode *current=head;
+//         ListNode *prev=NULL,*next=NULL;
+//         while(current!=NULL)
+//         {
+//             next=current->next;
+//             current->next=prev;
+//             prev=current;
+//             current=next;
+//         }
+//         head=prev;
+//         return head;
+//     }
     
     int getDecimalValue(ListNode* head) {
-        int sum=0,i=0;
-        ListNode *temp=reverse(head);
-        while(temp!=nullptr)
-        {
+//         int sum=0,i=0;
+//         ListNode *temp=reverse(head);
+//         while(temp!=nullptr)
+//         {
              
-             sum=sum+(temp->val)*pow(2,i);
-             i++;
-             temp=temp->next;
+//              sum=sum+(temp->val)*pow(2,i);
+//              i++;
+//              temp=temp->next;
+//         }
+//         return sum;
+        
+        
+        // -----------------------------------------------2nd approach ---------------------------------
+// first count the lenght of node then solve the binary to integer
+        
+        ListNode *temp=head;
+        int count=0;
+        while(temp!=NULL)
+        {
+            count++;
+            temp=temp->next;
+        }
+        int sum=0;
+        temp=head;
+        int i=1;
+        while(i<=count)
+        {
+            sum=sum+(temp->val)*pow(2,count-i);
+            temp=temp->next;
+            i++;
         }
         return sum;
+            
+         // --------------------------------------3rd approach------------------------------------
+        
+        
     }
 };
