284ddce4f1bfffced8c2dc393dd6389542dc86b7
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-02-22 23:42:03 +0530

    Time: 4 ms (27.37%), Space: 8.5 MB (55.22%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -12,22 +12,34 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         
-        int count=0;
-        ListNode *temp=head;
-        while(temp!=NULL)
-        {
-            count++;
-            temp=temp->next;
+//         int count=0;
+//         ListNode *temp=head;
+//         while(temp!=NULL)
+//         {
+//             count++;
+//             temp=temp->next;
             
-        }
+//         }
         
-       int half=count/2;
-        temp=head;
-        while(half--)
+//        int half=count/2;
+//         temp=head;
+//         while(half--)
+//         {
+//             temp=temp->next;
+//         }
+//         return temp;
+        
+// ------------------------------- without finding athe length-----------------------------------
+        
+/* in this approach take two pointe fast & slow like fast=2x slow=x, i mean fast cover 2 step and slow is cover 
+half of fast in this situation fast reached in end of node or slow cover mid of node;*/
+        
+        ListNode *slow=head,*fast=head;
+        while(fast!=NULL&&fast->next!=NULL)
         {
-            temp=temp->next;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        return temp;
-        
+        return slow;
     }
 };
