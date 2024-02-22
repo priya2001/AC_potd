3725043933bcccd1f42fcedb72e308b0d8f76187
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-02-22 18:12:16 +0530

    Time: 2 ms (46.74%), Space: 7.2 MB (10.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int climbStairs(int n) {
         /* this would be the first question of dp to be solve while leaarning the dp*/
-        if(n==1){
-            return 1;
+      if(n==1)
+      {
+          return 1;
+      }
+      int a[n+1];
+      a[1]=1;
+      a[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            a[i]=a[i-1]+a[i-2];
         }
-        int dp[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        return a[n];
     }
 };
