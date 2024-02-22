05bc720873d47cf9da7dcbf43524d4648f1d5021
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-02-23 01:27:28 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (39.67%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -45,26 +45,41 @@ public:
         // -----------------------------------------------2nd approach ---------------------------------
 // first count the lenght of node then solve the binary to integer
         
-        ListNode *temp=head;
-        int count=0;
-        while(temp!=NULL)
-        {
-            count++;
-            temp=temp->next;
-        }
-        int sum=0;
-        temp=head;
-        int i=1;
-        while(i<=count)
-        {
-            sum=sum+(temp->val)*pow(2,count-i);
-            temp=temp->next;
-            i++;
-        }
-        return sum;
+        // ListNode *temp=head;
+        // int count=0;
+        // while(temp!=NULL)
+        // {
+        //     count++;
+        //     temp=temp->next;
+        // }
+        // int sum=0;
+        // temp=head;
+        // int i=1;
+        // while(i<=count)
+        // {
+        //     sum=sum+(temp->val)*pow(2,count-i);
+        //     temp=temp->next;
+        //     i++;
+        // }
+        // return sum;
             
          // --------------------------------------3rd approach------------------------------------
+        // without reverse and length count
+/* first step-> assumed my first node is last Node pow(2,0) but we check first node is not a last node then we 
+come next node is pow(2,0) but our first node is pow(2,1)
+second step-> angin we assume the second node is last node but it is not true second Node pow(2,1)orfirst node
+pow(2,2);  this is contiune till meet last code
+ */
         
+        int ans=0;
+        ListNode *temp=head;
+        while(temp!=NULL)
+        {
+            ans=ans*2;
+            ans=ans+(temp->val);
+            temp=temp->next;
+        }
+        return ans;
         
     }
 };
