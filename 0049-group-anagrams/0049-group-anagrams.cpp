02ff1804771a458622ef67f6f9ab157fc556139c
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-04-06 16:49:37 +0530

    Time: 20 ms (90.85%), Space: 25 MB (31.75%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        int n=strs.size();
+        
+        vector<vector<string>>ans;
+        
+        map<string,vector<string>>mp;
+        
+        for(int i=0;i<n;i++)
+        {
+            string temp=strs[i];
+            
+            sort(temp.begin(),temp.end());
+            
+            mp[temp].push_back(strs[i]);
+        }
+        
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+};
