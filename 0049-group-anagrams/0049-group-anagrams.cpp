0862d71bb1d078fe21a7a72360c72143b01b55a2
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-04-06 17:17:25 +0530

    Time: 47 ms (11.30%), Space: 26 MB (21.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,67 @@
 class Solution {
 public:
+    
+    string generate(string &word)
+    { 
+       int arr[26]={0};
+        
+        for(char &ch:word)
+        {
+            arr[ch-'a']++;
+        }
+        string new_word="";
+        
+        for(int i=0;i<26;i++)
+        {
+            int freq =arr[i];
+            
+            if(freq>0)
+            {
+                new_word=new_word+string(freq,i+'a');
+            }
+        }
+        return new_word;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        int n=strs.size();
+//         int n=strs.size();
         
+//         vector<vector<string>>ans;
+        
+//         map<string,vector<string>>mp;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             string temp=strs[i];
+            
+//             sort(temp.begin(),temp.end());
+            
+//             mp[temp].push_back(strs[i]);
+//         }
+        
+//         for(auto it:mp)
+//         {
+//             ans.push_back(it.second);
+//         }
+//         return ans;
+        
+        //TC: n*klog(k)
+        // k : k is the maximum size of input
+        
+        // *******************************************2nd approcah without sorting***************************
+        
+        int n=strs.size();
         vector<vector<string>>ans;
         
         map<string,vector<string>>mp;
         
         for(int i=0;i<n;i++)
         {
-            string temp=strs[i];
+            string word=strs[i];
             
-            sort(temp.begin(),temp.end());
+            string new_word=generate(word);
             
-            mp[temp].push_back(strs[i]);
+            mp[new_word].push_back(word);
         }
         
         for(auto it:mp)
@@ -22,6 +69,5 @@ public:
             ans.push_back(it.second);
         }
         return ans;
-        
     }
 };
