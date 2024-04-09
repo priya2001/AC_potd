f5242a75de35542df304239c10c11241556b47a7
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-04-09 15:52:04 +0530

    Time: 168 ms (60.68%), Space: 98 MB (48.39%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,74 +1,103 @@
+// class Solution {
+// public:
+//     vector<int> sortArray(vector<int>& nums) 
+//     {
+//          //using sort function
+//         // sort(nums.begin(),nums.end());
+//         // return nums;
+        
+//         int n=nums.size();
+//         mergeSort(nums,n);
+//         return nums;
+        
+        
+//     }
+      
+//     void mergeSort(vector<int> &A, int n)
+//     {
+//         if (n < 2)
+//             return;
+//         int mid = n / 2;
+//         vector<int> L;
+//         vector<int> R;
+
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (i < mid)
+//             {
+//                 L.push_back(A[i]);
+//             }
+//             else
+//             {
+//                 R.push_back(A[i]);
+//             }
+//         }
+//         mergeSort(L, L.size());
+//         mergeSort(R, R.size());
+//         mergeArrays(A, L, R);
+//     }
+
+//     void mergeArrays(vector<int> &A, vector<int> &L, vector<int> &R)
+//     {
+
+//         int sizeL = L.size();
+//         int sizeR = R.size();
+//         int i = 0, j = 0, k = 0;
+
+//         while (i < sizeL && j < sizeR)
+//         {
+//             if (L[i] <= R[j])
+//             {
+//                 A[k] = L[i];
+//                 i++;
+//             }
+//             else
+//             {
+//                 A[k] = R[j];
+//                 j++;
+//             }
+//             k++;
+//         }
+//         while (i < sizeL)
+//         {
+//             A[k] = L[i];
+//             i++;
+//             k++;
+//         }
+//         while (j < sizeR)
+//         {
+//             A[k] = R[j];
+//             j++;
+//             k++;
+//         }
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) 
     {
-         //using sort function
-        // sort(nums.begin(),nums.end());
-        // return nums;
-        
         int n=nums.size();
-        mergeSort(nums,n);
-        return nums;
-        
-        
-    }
-      
-    void mergeSort(vector<int> &A, int n)
-    {
-        if (n < 2)
-            return;
-        int mid = n / 2;
-        vector<int> L;
-        vector<int> R;
-
-        for (int i = 0; i < n; i++)
+        unordered_map<int,int>mp;
+        for(auto &it:nums)
         {
-            if (i < mid)
-            {
-                L.push_back(A[i]);
-            }
-            else
-            {
-                R.push_back(A[i]);
-            }
+            mp[it]++;
         }
-        mergeSort(L, L.size());
-        mergeSort(R, R.size());
-        mergeArrays(A, L, R);
-    }
-
-    void mergeArrays(vector<int> &A, vector<int> &L, vector<int> &R)
-    {
-
-        int sizeL = L.size();
-        int sizeR = R.size();
-        int i = 0, j = 0, k = 0;
-
-        while (i < sizeL && j < sizeR)
+        
+        int i=0;
+        int minE=*min_element(begin(nums),end(nums));
+        int maxE=*max_element(begin(nums),end(nums));
+        
+        for(int num=minE;num<=maxE;num++)
         {
-            if (L[i] <= R[j])
+            while(mp[num]>0)
             {
-                A[k] = L[i];
+                nums[i]=num;
                 i++;
+                mp[num]--;
             }
-            else
-            {
-                A[k] = R[j];
-                j++;
-            }
-            k++;
         }
-        while (i < sizeL)
-        {
-            A[k] = L[i];
-            i++;
-            k++;
-        }
-        while (j < sizeR)
-        {
-            A[k] = R[j];
-            j++;
-            k++;
-        }
+        return nums;
     }
 };
