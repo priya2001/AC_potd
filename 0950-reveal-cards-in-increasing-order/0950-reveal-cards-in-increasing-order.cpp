0cc0e5997060a6dd0e38a98ed194717e732b7cb8
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-04-10 19:32:33 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (67.85%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -2,26 +2,55 @@ class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         
+        // approach 1 TC :o(nlog(n))
+        
+//         int n=deck.size();
+//         vector<int>result(n,0);
+        
+//         sort(deck.begin(),deck.end());  //remember its important
+//         int i=0;//deck
+//         int j=0;//result
+        
+//         bool skip=false;
+//         while(i<n)
+//         {
+//             if(result[j]==0)
+//             {
+//                 if(skip==false) //alternate
+//                  {
+//                      result[j]=deck[i];
+//                      i++;
+//                   }
+//                    skip= !skip;
+//             }
+//             j=(j+1)%n;
+//         }
+//         return result;
+        
+          // approach 2  using queue
+        
         int n=deck.size();
+        queue<int>q;
         vector<int>result(n,0);
         
-        sort(deck.begin(),deck.end());  //remember its important
-        int i=0;//deck
-        int j=0;//result
+        sort(begin(deck),end(deck));
         
-        bool skip=false;
-        while(i<n)
+        for(int i=0;i<n;i++)
         {
-            if(result[j]==0)
+            q.push(i);
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            int indx=q.front();  //store index
+            q.pop();
+            result[indx]=deck[i]; //aggin value
+            
+            if(!q.empty())
             {
-                if(skip==false) //alternate
-                 {
-                     result[j]=deck[i];
-                     i++;
-                  }
-                   skip= !skip;
+                q.push(q.front());   //first element push in last
+                q.pop();
             }
-            j=(j+1)%n;
         }
         return result;
     }
