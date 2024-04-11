60b47878aac0491c83d9c0a4a9cd05f2759ab09f
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-04-11 16:44:26 +0530

    Time: 8 ms (5.26%), Space: 11.9 MB (86.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        //brute force approach  
-      for(int i = 0; i< matrix.size();i++)
-      {
-            for(int j = 0; j<matrix[0].size();j++)
+          int m = matrix.size();
+        if (m == 0) return false; // Empty matrix
+        
+        int n = matrix[0].size();
+        if (n == 0) return false; // Empty row
+        
+        // Start from top-right corner
+        int row = 0, col = n - 1;
+        
+        while (row < m && col >= 0)
+        {
+            if (matrix[row][col] == target)
             {
-                if(matrix[i][j]==target)
-                {
-                    return true;
-                }
+                return true; // Found target
+            } 
+            else if (matrix[row][col] < target)
+            {
+                row++; // Move down (because elements in the current column are smaller)
+            }
+            else
+            {
+                col--; // Move left (because elements in the current row are larger)
             }
         }
-        return false;
+        
+        return false; // Target not found
     }
 };
