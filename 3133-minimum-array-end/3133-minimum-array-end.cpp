2fe489174be110447f8c0499ce39377af349bdfb
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-04-29 00:22:23 +0530

    Time: 1726 ms (10.00%), Space: 7.8 MB (90.00%) - LeetHub

---
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long ans=x;
+        for(int i=0;i<n-1;i++)
+        {
+            ans++;
+            ans=ans|x;
+        }
+        return ans;
+    }
+};
