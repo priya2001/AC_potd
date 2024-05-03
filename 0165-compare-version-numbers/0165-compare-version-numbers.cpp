43119139933bc339c0e1d19303d76cbc61ca858c
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-05-03 09:54:36 +0530

    Time: 3 ms (39.03%), Space: 8.1 MB (5.03%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        int i=0,j=0,v1=version1.size(),v2=version2.size();
-        while(i<v1||j<v2)
+     vector<string> getTokens(string version) 
+     {
+        stringstream v(version);
+        string token_v = "";
+        vector<string> version_tokens;
+        while(getline(v, token_v, '.'))
         {
-            int n1=0,n2=0;
-            while(i<v1 && version1[i]!='.')
-            {
-                n1=n1*10+(version1[i]-'0');
-                i++;
-            }
-            while(j<v2&& version2[j]!='.')
-            {
-                n2=n2*10+(version2[j]-'0');
-                j++;
-            }
-            if(n1<n2)
-            {
-                return -1;
-            }
-            else if(n1>n2)
-            {
+            version_tokens.push_back(token_v);
+        }
+        return version_tokens;
+    }
+    int compareVersion(string version1, string version2)
+    {
+        
+        vector<string> v1 = getTokens(version1);
+        vector<string> v2 = getTokens(version2);  
+        
+        int m = v1.size();
+        int n = v2.size();
+            
+        int  i = 0;
+        while(i < m || i < n) {
+            
+            int a = i < m ? stoi(v1[i]) : 0 ;
+            int b = i < n ? stoi(v2[i])  : 0 ;
+            
+            if(a > b)
                 return 1;
-            }
+            else if(b > a)
+                return -1;
             else
-            {
                 i++;
-                j++;
-            }
         }
         return 0;
+        
     }
 };
