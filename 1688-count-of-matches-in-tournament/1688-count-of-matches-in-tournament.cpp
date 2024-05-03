5b1d2bee958ec94a65ab3e131be5fb3b58ef66e1
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-05-03 16:25:22 +0530

    Time: 1 ms (45.20%), Space: 7.1 MB (46.30%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,15 @@
+//Approach-2 (Using simple math)
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
+// class Solution {
+// public:
+//     int numberOfMatches(int n) {
+//         return n-1;
+//     }
+// };
