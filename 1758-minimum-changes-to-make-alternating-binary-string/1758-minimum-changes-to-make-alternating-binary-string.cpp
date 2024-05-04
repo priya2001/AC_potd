c2ffb9581e41e36d0f980fe5abedb7aaaa66519b
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-05-05 01:26:12 +0530

    Time: 3 ms (80.66%), Space: 8 MB (96.70%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -3,34 +3,26 @@ public:
     int minOperations(string s) {
         int n=s.size();
         
-        int start_with_0 = 0;
-        int start_with_1 = 0;
+        int start_with_0 = 0; //0101010..
+        int start_with_1 = 0; //10101010....
         
         for(int i=0;i<n;i++)
         {
             if(i%2==0)
             {
-                if(s[i]=='0')
-                {
-                    start_with_1++;
-                }
-                else
-                {
-                    start_with_0++;    
-                }
+               if(s[i]=='0')
+               {
+                   start_with_0++;
+               }
             }
             else
             {
-                if(s[i]=='1')
-                {
-                    start_with_1++;
-                }
-                else
-                {
-                    start_with_0++;
-                }
+               if(s[i]=='1')
+               {
+                   start_with_0++;
+               }
             }
         }
-        return min(start_with_0,start_with_1);
+        return min(start_with_0,n-start_with_0);
     }
 };
