3078e70568db4d582aa61996470d120aa44590e0
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-05-05 23:23:57 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (36.77%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int ans=1;
-        for(int i=0;i<=30;i++)
-        {
-            
-            if(ans==n)
-            {
-                return true;
-            }
-            if(ans<INT_MAX/2)
-            {
-                ans=ans*2;
-            }
-        }
-        return false;
         
+        if(n<=0)
+        {
+            return false;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        return n%2==0 && isPowerOfTwo(n/2);
     }
 };
