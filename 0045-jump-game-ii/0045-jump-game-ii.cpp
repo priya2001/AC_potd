c3eaa63280eba95df65b5a7a2e15d1c13e2c6de2
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-06-03 20:00:01 +0530

    Time: 8 ms (82.48%), Space: 19 MB (34.48%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=1;i<n;i++)
+        {
+            nums[i]=max(nums[i]+i,nums[i-1]);
+        }
+
+        int jump=0;
+        int i=0;
+        while(i<n-1)
+        {
+            jump++;
+            i=nums[i];
+
+        }
+        return jump;
+    }
+};
