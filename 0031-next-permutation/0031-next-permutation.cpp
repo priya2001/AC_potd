d0b2e7bffbd326ee4b69bd122dfd7af5c4d4ce32
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-06-10 20:11:19 +0530

    Time: 4 ms (56.37%), Space: 14.7 MB (14.73%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,36 +1,8 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        
-        // find the blue gola
-        int gola_index=-1;
-        for(int i=n-1;i>0;i--)
-        {
-            if(nums[i]>nums[i-1])
-            {
-                gola_index=i-1;
-                break;
-            }
-        }
-        
-        // find swap index
-        if(gola_index!=-1) // yeh condition false hogi to direct reverse ho jayega 
-        {
-        int swap_index=gola_index;
-        for(int j=n-1;j>=gola_index+1;j--)
-        {
-            if(nums[j]>nums[gola_index])
-            {
-                swap_index=j;
-                break;
-            }
-        }
-        swap(nums[swap_index],nums[gola_index]); 
-        }
-       
-        reverse(nums.begin() + gola_index + 1,nums.end()); //reverse after gola_index
-        
+        next_permutation(nums.begin(),nums.end());
+    
         
     }
 };
