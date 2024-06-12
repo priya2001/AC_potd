bfbdcd8466acf5c7e248e94b682ac472cc04dbfa
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-06-13 01:17:09 +0530

    Time: 2 ms (44.12%), Space: 9.9 MB (34.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,65 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // ###################brute force approach
-        // sort(nums.begin(),nums.end());
-        
-        
-        // **************************better approach  O(2N) time complexity
-//         int count0=0,count1=0,count2=0;
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             if(nums[i]==0)
-//             {
-//                 count0++;
-//             }
-//             else if(nums[i]==1)
-//             {
-//                 count1++;
-//             }
-//             else
-//             {
-//                  count2++; 
-//             }
-//         }
-//         for(int i=0;i<count0;i++)
-//         {
-//             nums[i]=0;
-//         }
-//         for(int i=count0;i<count0+count1;i++)
-//         {
-//             nums[i]=1;
-//         }
-//         for(int i=count0+count1;i<nums.size();i++)
-//         {
-//             nums[i]=2;
-//         }
-        
-//         }
-        
-    // **********************************Optimal appraoch O(N)***************************** 
-        
-        int low=0,mid=0,high=nums.size()-1;
-        while(mid<=high)
+        int n=nums.size();
+        int i=0;
+        int j=0;
+        int k=n-1;
+        while(j<=k)
         {
-            if(nums[mid]==0)
+            if(nums[j]==1) 
             {
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
+                j++;
             }
-            else if(nums[mid]==1)
+            else if(nums[j]==0)
             {
-                mid++;
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
             }
             else
             {
-                swap(nums[mid],nums[high]);
-                high--;
+                swap(nums[j],nums[k]);
+                k--;
             }
         }
-        
-        
     }
-        
 };
