f6247d53c33f6e886bd89d89f570f56043acef34
priya2001 <39214101+priya2001@users.noreply.github.com>
2024-06-12 19:05:03 +0530

    Time: 0 ms (100%), Space: 14.6 MB (14.89%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int>& nums) 
+    {
         next_permutation(nums.begin(),nums.end());
-    
-        
     }
 };
